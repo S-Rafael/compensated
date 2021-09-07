f9201bb071d0645fa0e0b76d40083a7b3ab8cd3b
Rafael Siejakowski <rs@rs-math.net>
2021-09-07 13:08:33 -0300

    Implement complex and generic cases of Kahan summation

--- knlite.hpp
+++ knlite.hpp
@@ -1,4 +1,4 @@
-/* encoding: UTF-8
+/** encoding: UTF-8
  *
  * © Copyright 2021 Rafał M. Siejakowski <rs@rs-math.net>
  *
@@ -26,8 +26,8 @@
 namespace kn
 {
 /*
- * First, we formulate the concept of "admissible" raw value types and two
- * special predicates:
+ * First, we formulate the concept of "admissible" raw value types
+ * and introduce two special predicates:
  *
  * • being "real"    (a real number in the mathematical sense),
  * • being "complex" (a complex number in the mathematical sense).
@@ -37,44 +37,30 @@ namespace kn
  *
  * 1) It must be assignable from the literal `0`.
  * 2) There must be an operator + and operator - defined for the raw type,
- *    both of which return a type convertible to the raw value type in question.
+ *    both of which return a type convertible to the raw value type.
  *
  * Optionally, if the raw value type is "real" or "complex", then we
  * can use Neumaier's algorithm which improves on Kahan's.
- * The improved (Kahan-Neumaier) algorithm keeps the running compensation small.
+ * The improved (Kahan-Neumaier) algorithm keeps the running compensation
+ * small.
  */
 
 /**
- * @brief Whether the asignment from the literal `0` is syntactically correct
+ * @brief Whether the basic Kahan summation algorithm can be implemented for the type
  */
 template<typename T>
-concept assignable_from_zero_literal = requires(T a) {a = 0;};
-
-/**
- * @brief Whether a binary operator `+` exists for the type
- */
-template<typename T>
-concept has_binary_plus  = requires(T a, T b, T result) {result = a + b;};
-
-/**
- * @brief Whether a binary operator `-` exists for the type
- */
-template<typename T>
-concept has_binary_minus = requires(T a, T b, T result) {result = a - b;};
+concept kahanizable = requires(T a, T b)
+{
+	a = 0; // Assignable from zero
+	{a + b} -> std::convertible_to<T>; // has a binary plus
+	{a - b} -> std::convertible_to<T>; // has a binary minus
+};
 
 /**
  * @brief Whether a unary operator `-` exists for the type
  */
 template<typename T>
 concept has_unary_minus = requires(T a, T result) {result = -a;};
-
-/**
- * @brief Whether the basic Kahan summation algorithm can be implemented for the type
- */
-template<typename T>
-concept is_admissible = assignable_from_zero_literal<T>
-						&& has_binary_plus<T>
-						&& has_binary_minus<T>;
 
 /**
  * @brief Whether the type has an overload of std::abs
@@ -101,63 +87,64 @@ template<typename T>
 concept is_real = std::three_way_comparable<T>
 				  && (has_std_abs<T> || has_custom_abs<T>);
 
-/* Formulate predicates that determine whether a type behaves
+/*
+ * Formulate predicates that determine whether a type behaves
  * "like a complex number", i.e., it has public member functions
  * real(), imag() returning admissible real types and has a
  * two-argument constructor which accepts these types.
  */
 template<typename T>
-concept admissible_real = is_admissible<T> && is_real<T>;
-
-template<typename T>
-concept has_real_part = requires(T z)
-{
-	{z.real()} -> admissible_real;
-};
-
-template<typename T>
-concept has_imag_part = requires(T z)
-{
-	{z.imag()} -> admissible_real;
-};
-
-template<typename T>
-concept has_reconstructor = requires(T z) {z = T(z.real(), z.imag());};
+concept std_real = kahanizable<T>
+				   && std::three_way_comparable<T>
+				   && has_std_abs<T>;
 
 /**
  * @brief Whether the type represents a complex number
  */
 template<typename T>
-concept is_complex = has_real_part<T>
-					 && has_imag_part<T>
-					 && has_reconstructor<T>;
+concept is_complex = requires(T z)
+{
+	{z.real()} -> std_real; // Has a real part
+	{z.imag()} -> std_real;	// Has an imaginary part
+	z = T(z.real(), z.imag()); // Can be reconstructed from those
+};
 //=============================================================================================
 /**
  * @mainclass
- * class `value` - template class for elements with Kahan-Neumaier addition.
+ * class `value` - template class representing a value
+ * with compensated Kahan/Kahan-Neumaier addition.
+ * @param
+ * The template parameter is the underlying "raw" value.
  */
 template<typename V>
-requires is_admissible<V>
+requires kahanizable<V>
 class value
 {
 private:
-	const V zero = 0; 	    // This is a constant, so a static member.
+	static const V zero = 0;
 	// There are only two private members that actually live in the object:
 	V Sum = zero;           // the sum
 	V Compensation = zero;  // the running compensation
+
 public:
 	// Constructors from nothing and from V:
 	value() = default;
 	value(V  initial_value) : Sum{initial_value} {Compensation = zero;}
 	value(V& initial_value) : Sum{initial_value} {Compensation = zero;}
-	// Copy/move constructors and assignment operators – defaulted.
+	// Copy/move constructors and assignment operators: all defaulted.
 	~value() = default;
+
 private:
 	// constructor which manually sets the members: for internal use only.
 	value(V S, V C) : Sum{S}, Compensation{C} {};
+
 public:
 //=== Conversion operators ===
+	/**
+	 * @brief Conversion operator to the raw value type
+	 */
 	inline operator V() const {return Sum + Compensation;}
+
 //=== Equality comparison operators ===
 	/**
 	 * @brief operator== tries to determine if two objects represent the
@@ -169,10 +156,12 @@ public:
 	{
 		return (Sum - other.Sum == other.Compensation - Compensation);
 	}
+
 	inline bool operator!= (const value<V>& other) const
 	{
 		return !(operator==(other));
 	}
+
 	/**
 	 * @brief operator== tries to compare the value represented by this
 	 * object with a raw value
@@ -183,10 +172,12 @@ public:
 	{
 		return (Compensation == value - Sum) || (Sum == value - Compensation);
 	}
+
 	inline bool operator!= (const V& value) const
 	{
 		return !(operator==(value));
 	}
+
 //=== Unary minus ===
 	/**
 	 * @brief Unary minus - for raw value types possessing a unary minus
@@ -196,6 +187,7 @@ public:
 	{
 		return value<V>(-Sum, -Compensation);
 	}
+
 	/**
 	 * @brief Unary minus - for raw value types without a unary minus
 	 */
@@ -205,10 +197,12 @@ public:
 		// We use subtraction from zero since there is no unary minus for V
 		return value<V>(zero - Sum, zero - Compensation);
 	}
-// === Kahan summation operators (on the right) ===
+
+// === Kahan-Neumaier summation operators (on the right) ===
 // --- Real case ---
 	/**
-	 * @brief Add an element of type V (real case supported by std::abs)
+	 * @brief Add an element of type V using the Kahan-Neumaier addition
+	 * (real case supported by std::abs)
 	 */
 	inline value<V> operator+ (const V& increment) const
 	requires is_real<V> && has_std_abs<V>
@@ -232,8 +226,10 @@ public:
 							Compensation + (increment - naive_sum) + Sum);
 		}
 	}
+
 	/**
-	 * @brief Add an element of type V (real case with user-supplied abs() member)
+	 * @brief Add an element of type V using the Kahan-Neumaier addition
+	 * (real case with user-supplied abs() member)
 	 */
 	inline value<V> operator+ (const V& increment) const
 	requires is_real<V> && has_custom_abs<V> && (!has_std_abs<V>)
@@ -246,8 +242,10 @@ public:
 			return value<V>(naive_sum,
 							Compensation + (increment - naive_sum) + Sum);
 	}
+
 	/**
-	 * @brief Increment in-place by an element of type V (real case supported by std::abs)
+	 * @brief Add in-place an element of type V using the Kahan-Neumaier addition
+	 * (real case supported by std::abs)
 	 */
 	inline void operator+= (const V& increment)
 	requires is_real<V> && has_std_abs<V>
@@ -259,9 +257,10 @@ public:
 			Compensation = Compensation + (increment - naive_sum) + Sum;
 		Sum = naive_sum;
 	}
+
 	/**
-	 * @brief Increment in-place by an element of type V (real case with user-supplied
-	 * abs() member)
+	 * @brief Add in-place an element of type V using the Kahan-Neumaier addition
+	 * (real case with user-supplied abs() member)
 	 */
 	inline void operator+= (const V& increment)
 	requires is_real<V> && has_custom_abs<V> && (!has_std_abs<V>)
@@ -273,10 +272,93 @@ public:
 			Compensation = Compensation + (increment - naive_sum) + Sum;
 		Sum = naive_sum;
 	}
+
 // --- Complex case
+	/**
+	 * @brief Add an element of type V using the Kahan-Neumaier addition
+	 * (complex case)
+	 */
+	inline value<V> operator+ (const V& increment) const
+	requires is_complex<V>
+	{
+		V naive_sum = Sum + increment;
+		auto inc_real = increment.real();
+		auto inc_imag = increment.imag();
 
-// --- The case of neither real nor complex V
+		// Real and imaginary parts of the update to Compensation:
+		decltype(inc_real) comp_update_real, comp_update_imag;
 
+		// Compute the update to the real part of the compensation
+		if (std::abs(Sum.real()) > std::abs(inc_real))
+			comp_update_real = (Sum.real() - naive_sum.real()) + inc_real;
+		else
+			comp_update_real = (inc_real - naive_sum.real()) + Sum.real();
+
+		// Compute the update to the imaginary part of the compensation
+		if (std::abs(Sum.imag()) > std::abs(inc_imag))
+			comp_update_imag = (Sum.imag() - naive_sum.imag()) + inc_imag;
+		else
+			comp_update_imag = (inc_imag - naive_sum.imag()) + Sum.imag();
+
+		return value<V>(naive_sum,
+						Compensation + V(comp_update_real, comp_update_imag));
+	}
+
+	/**
+	 * @brief Add in-place an element of type V using the Kahan-Neumaier addition
+	 * (complex case)
+	 */
+	inline void operator+= (const V& increment)
+	requires is_complex<V>
+	{
+		V naive_sum = Sum + increment;
+		auto inc_real = increment.real();
+		auto inc_imag = increment.imag();
+
+		// Real and imaginary parts of the update to Compensation:
+		decltype(inc_real) comp_update_real, comp_update_imag;
+
+		// Compute the update to the real part of the compensation
+		if (std::abs(Sum.real()) > std::abs(inc_real))
+			comp_update_real = (Sum.real() - naive_sum.real()) + inc_real;
+		else
+			comp_update_real = (inc_real - naive_sum.real()) + Sum.real();
+
+		// Compute the update to the imaginary part of the compensation
+		if (std::abs(Sum.imag()) > std::abs(inc_imag))
+			comp_update_imag = (Sum.imag() - naive_sum.imag()) + inc_imag;
+		else
+			comp_update_imag = (inc_imag - naive_sum.imag()) + Sum.imag();
+
+		// Update in-place:
+		Sum = naive_sum;
+		Compensation = Compensation + V(comp_update_real, comp_update_imag);
+	}
+
+// --- The case of V neither real nor complex - plain Kahan algorithm
+	/**
+	 * @brief Add an element of type V (neither real nor complex)
+	 * using Kahan summation
+	 */
+	inline value<V> operator+ (const V& increment) const
+	requires (!is_real<V>) && (!is_complex<V>)
+	{   // plain Kahan
+		V naive_sum = Sum + increment;
+		return value<V>(naive_sum,
+						Compensation + (Sum - naive_sum) + increment);
+	}
+
+	/**
+	 * @brief Add in-place an element of type V (neither real nor complex)
+	 * using Kahan summation
+	 */
+	inline void operator+= (const V& increment) const
+	requires (!is_real<V> && !is_complex<V>)
+	{   // plain Kahan
+		V naive_sum = Sum + increment;
+		Compensation = Compensation + (Sum - naive_sum) + increment;
+		Sum = naive_sum;
+	}
 
 // --- Operators that are common to all cases
 	/**
@@ -288,6 +370,7 @@ public:
 		result += other.Compensation;
 		return result;
 	}
+
 	/**
 	 * @brief Adds in-place an element of the same type
 	 */
@@ -296,42 +379,60 @@ public:
 		operator+=(other.Sum);
 		operator+=(other.Compensation);
 	}
+
 // ------ Operator minus is defined in terms of the above and unary minus:
 	inline value<V> operator- (const V& increment) const
+	requires has_unary_minus<V>
+	{
+		return operator+(-increment);
+	}
+
+	inline value<V> operator- (const V& increment) const
+	requires (!has_unary_minus<V>)
 	{
 		return operator+(zero-increment);
 	}
+
 	inline void operator-= (const V& increment)
+	requires has_unary_minus<V>
+	{
+		operator+=(-increment);
+	}
+
+	inline void operator-= (const V& increment)
+	requires (!has_unary_minus<V>)
 	{
 		operator+=(zero-increment);
 	}
+
 	inline value<V> operator- (const value<V>& other) const
 	{
 		return operator+(-other);
 	}
+
 	inline void operator-= (const value<V>& other)
 	{
 		operator+=(-other);
 	}
 }; // class value
 
-// == Left operators: V + value<V>, V - value<V> ==
+// ==== Left operators: V + value<V>, V - value<V> ====
 /**
  * @brief Operator `+` for adding a raw value on the left
  */
 template<typename V>
-requires is_admissible<V>
-value<V> operator+(V raw, value<V> kn)
+requires kahanizable<V>
+inline value<V> operator+(V raw, value<V> kn)
 {
-	return kn + raw; // Addition is commutative
+	return kn + raw;
 }
 
 /**
  * @brief Operator `-` for subtracting from a raw value
  */
 template<typename V>
-requires is_admissible<V>
-value<V> operator-(V raw, value<V> kn)
+requires kahanizable<V>
+inline value<V> operator-(V raw, value<V> kn)
 {
 	return (-kn) + raw;
 }
