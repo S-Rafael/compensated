843ec6a3dba3d8371de7bc4315e139f3f9571d34
Rafael Siejakowski <rs@rs-math.net>
2021-09-08 23:41:18 -0300

    Add many more tests, including for custom raw value types

---
+++ tests/test_classes.h
@@ -0,0 +1,86 @@
+/** encoding: UTF-8
+ *
+ * © Copyright 2021 Rafał M. Siejakowski <rs@rs-math.net>
+ *
+ * This software is licensed under the terms of the 3-Clause BSD License.
+ * Please refer to the accompanying LICENSE file for the license terms.
+ *
+==========================================================
+
+	Definitions of raw value classes used in tests
+
+*/
+#ifndef _TEST_CLASSES_H
+#define _TEST_CLASSES_H
+
+/**
+ * @brief The real_with_custom_abs class provides an example
+ * of a simple custom type which satisfies the constraints.
+ */
+class real_with_custom_abs
+{
+	double x;
+public:
+	real_with_custom_abs(double v) : x{v} {};
+	inline double abs() const {return std::abs(x);};
+	inline real_with_custom_abs operator+ (const real_with_custom_abs& other) const
+	{
+		return real_with_custom_abs(x + other.x);
+	}
+	inline real_with_custom_abs operator- (const real_with_custom_abs& other) const
+	{
+		return real_with_custom_abs(x - other.x);
+	}
+	inline bool is_zero() const {return x == 0.0;}
+};
+
+/**
+ * @brief The custom_complex class provides a test case for a user-supplied
+ * raw value type with member functions real() and imag()
+ */
+class custom_complex
+{
+	float x, y;
+public:
+	custom_complex(float real, float imag) : x{real}, y{imag} {};
+	custom_complex(int n) : y{0} { x=static_cast<float>(n); };
+	inline float real() const {return x;}
+	inline float imag() const {return y;}
+	inline custom_complex operator+ (const custom_complex& other) const
+	{
+		return custom_complex(x + other.x, y + other.y);
+	}
+	inline custom_complex operator- (const custom_complex& other) const
+	{
+		return custom_complex(x - other.x, y - other.y);
+	}
+};
+
+/**
+ * @brief The custom_gadget class provides a generic type, neither real nor
+ * complex, which can be operated on via the Kahan algorithm
+ */
+class custom_gadget
+{
+	float x, y, z;
+public:
+	custom_gadget(float nx, float ny, float nz) : x{nx}, y{ny}, z{nz} {};
+	custom_gadget(int n) : x{static_cast<float>(n)},
+						   y{static_cast<float>(n)},
+						   z{static_cast<float>(n)} {};
+	inline custom_gadget operator+ (const custom_gadget& other) const
+	{
+		return custom_gadget(x + other.x, y + other.y, z + other.z);
+	}
+	inline custom_gadget operator- (const custom_gadget& other) const
+	{
+		return custom_gadget(x - other.x, y - other.y, z - other.z);
+	}
+	bool operator== (const custom_gadget& other) const
+	{
+		return (x == other.x) && (y == other.y) && (z == other.z);
+	}
+};
+
+#endif // _TEST_CLASSES_H
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fenc=utf-8 :
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -12,10 +12,10 @@
 */
 
 #include <complex>
-#include <iostream>
-
 #include "gtest/gtest.h"
 #include "../knlite.hpp"
+
+#include "test_classes.h"
 
 /**
  * @class The knlite_test class inherits from Google testing
@@ -42,13 +42,13 @@ public:
 		}
 
 		// Construct the huge and tiny float
-		size_t half_bitsize_of_float = sizeof(float);
+		size_t half_bitsize_of_float = 4 * sizeof(float);
+		huge_fl = tiny_fl = 1.0;
 		for (unsigned p=0; p < half_bitsize_of_float; p++)
 		{
 			huge_fl *= 2.0; // make huge_fl at least 2^(mantissa)
 			tiny_fl *= 0.5; // mage tiny_fl at most 2^(-mantissa)
 		}
-
 		::testing::Test::SetUpTestCase();
 	}
 
@@ -60,6 +60,23 @@ public:
 		::testing::Test::TearDownTestCase();
 	}
 };
+
+//============================================================================================
+
+/**
+ * @test Check if the precision is lost with ordinary operations
+ */
+TEST_F(knlite_test, testcase_sanity)
+{
+	// Try losing precision with doubles...
+	double test1 = huge_dbl + tiny_dbl - huge_dbl - tiny_dbl;
+	ASSERT_FALSE(test1 == 0.0);
+
+	// ... and with floats
+	float test2 = huge_fl + tiny_fl - huge_fl - tiny_fl;
+	ASSERT_FALSE(test2 == 0.0);
+}
+
 
 /**
  * @test Check if the size of object is twice the size of raw value
@@ -89,6 +106,11 @@ TEST_F(knlite_test, conversions)
 	double dk = (double) k;
 	kn::value<double> kdk{dk};
 	EXPECT_EQ(k, kdk);
+
+	// Test assignment and conversion operators
+	k = 1.0;
+	dk = k;
+	EXPECT_DOUBLE_EQ(dk, 1.0);
 
 	// Test conversions by extracting real and imaginary parts
 	// of a complex number
@@ -146,6 +168,150 @@ TEST_F(knlite_test, unary_minus)
 	EXPECT_EQ(fsum, 0.0);
 }
 
+/**
+ * @test Test arithmetic operations on floats
+ */
+TEST_F(knlite_test, kahan_neumaier_float)
+{
+	// Test adding floats
+	kn::value<float> v{huge_fl};
+	v += tiny_fl;
+	v -= huge_fl;
+	v -= tiny_fl;
+	float result{v};
+	EXPECT_FLOAT_EQ(result, 0.0f);
+
+	// Test adding floats on the left
+	v = huge_fl;
+	v = tiny_fl + v;
+	v = (-huge_fl) + v;
+	v = (-tiny_fl) +v;
+	result = v;
+	EXPECT_FLOAT_EQ(result, 0.0f);
+}
+
+/**
+ * @test Test arithmetic operations on doubles
+ */
+TEST_F(knlite_test, kahan_neumaier_double)
+{
+	// Test adding doubles
+	kn::value<double> v{huge_dbl};
+	v += tiny_dbl;
+	v = v - huge_dbl;
+	v = v - tiny_dbl;
+	double result{v};
+	EXPECT_DOUBLE_EQ(result, 0.0f);
+
+	// Test adding doubles on the left
+	v = huge_dbl;
+	v = tiny_dbl + v;
+	v = (-huge_dbl) + v;
+	v = (-tiny_dbl) + v;
+	result = v;
+	EXPECT_DOUBLE_EQ(result, 0.0f);
+}
+
+/**
+ * @test Test arithmetic operations on complex numbers
+ */
+TEST_F(knlite_test, kahan_neumaier_complex)
+{
+	// Test Kahan-Neumaier algorithm for complex numbers
+	std::complex<double> z {huge_dbl, tiny_dbl};
+	std::complex<double> w {tiny_dbl, huge_dbl};
+	kn::value kz {z};
+	kn::value kw {w};
+	auto result = kz + kw - kz - kw;
+	EXPECT_DOUBLE_EQ(result.real(), 0.0);
+	EXPECT_DOUBLE_EQ(result.imag(), 0.0);
+
+	// Test operators on the right
+	kn::value b {z};
+	std::complex<double> sum = ((b + w) - z) - w;
+	EXPECT_DOUBLE_EQ(sum.real(), 0.0);
+	EXPECT_DOUBLE_EQ(sum.imag(), 0.0);
+
+	// Test operators on the right
+	b = z;
+	sum = w + (-z + (-w + b));
+	EXPECT_DOUBLE_EQ(sum.real(), 0.0);
+	EXPECT_DOUBLE_EQ(sum.imag(), 0.0);
+
+	// Test in-place addition
+	b = z;
+	b += w;
+	b -= z;
+	b -= w;
+	sum = b;
+	EXPECT_DOUBLE_EQ(sum.real(), 0.0);
+	EXPECT_DOUBLE_EQ(sum.imag(), 0.0);
+}
+
+/**
+ * @test Test the Kahan-Neumaier summation with a custom value
+ * type representing a real number, with user-supplied abs() member
+ */
+TEST_F(knlite_test, custom_real_type)
+{
+	real_with_custom_abs x = 0;
+	real_with_custom_abs h = huge_dbl;
+	real_with_custom_abs t = tiny_dbl;
+	kn::value<real_with_custom_abs> kx {x};
+
+	// Test right operations
+	kx = kx + h;
+	kx = kx + t;
+	kx = kx - h;
+	kx = kx - t;
+	real_with_custom_abs result = kx;
+	EXPECT_TRUE(result.is_zero());
+
+	// Test mixed operations
+	kx = x;
+	kx = h + kx;
+	kx = t + kx;
+	kx = kx - h;
+	kx = kx - t;
+	result = kx;
+	EXPECT_TRUE(result.is_zero());
+}
+
+/**
+ * @test Test the Kahan-Neumaier summation with a custom complex-like type
+ */
+TEST_F(knlite_test, custom_complex_type)
+{
+	custom_complex z(huge_fl, tiny_fl);
+	custom_complex w(tiny_fl, huge_fl);
+	kn::value test{z};
+	test = w + test;
+	test = test - z;
+	test -= w;
+	EXPECT_FLOAT_EQ(test.real(), 0.0);
+	EXPECT_FLOAT_EQ(test.imag(), 0.0);
+}
+
+/**
+ * @test Test the pure Kahan summation with a custom general type
+ */
+TEST_F(knlite_test, custom_general_type)
+{
+	// Make huge that we lose precision adding tiny_fl to huge_fl
+	EXPECT_FLOAT_EQ(huge_fl + tiny_fl, huge_fl);
+
+	// Check that we do NOT lose precision with kn::value
+	custom_gadget all_huge(huge_fl, huge_fl, huge_fl);
+	custom_gadget all_tiny(tiny_fl, tiny_fl, tiny_fl);
+
+	kn::value test{all_huge};
+	kn::value<custom_gadget> larger = test + all_tiny;
+	EXPECT_FALSE(test == larger);
+}
+
+/**
+ * @brief main function of the test suite
+ */
 int main(int argc, char** argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
